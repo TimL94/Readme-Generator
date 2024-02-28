f4322a86fa751fa2b63d350bb44b3c28c6040996
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-02-28 17:42:20 -0500

    included packages, installed inquirer and created quetions

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter project Title',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter project description',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter instalation notes',
+        name: 'instalation'
+    },
+    {
+        type: 'input',
+        message: 'Enter project usage details',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter Github Username',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Enter Contributers',
+        name: 'contributers'
+    },
+    {
+        type: 'list',
+        message: 'Choose your projects license',
+        name: 'license',
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'GNU General Public License v3.0',
+            'Boost Software license 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License'
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    generateMarkdown(data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        let {title} = data;
+        writeToFile(title, data);
+        
+    });
+}
 
 // Function call to initialize app
 init();
